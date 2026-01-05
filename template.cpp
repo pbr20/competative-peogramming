748e013d35c9279f800db900041b39779f9e09ae
pbr20 <rithen1971@gmail.com>
2026-01-05 20:52:55 +0600

    template updated

--- template.cpp
+++ template.cpp
@@ -4,12 +4,20 @@ using namespace std;
     #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     #define endl '\n'                               //cmnt out after semicolon if u want interactive output (one by one)
     #define lli long long int
+
+    bool cmp(pair<int,int> &p1, pair<int,int> &p2);
+
     int main(){
     optimize();
     freopen("input.txt", "r" , stdin);
     freopen("output.o" , "w" , stdout);
     
-
-
+      
     return 0;
 }
+
+    bool cmp(pair<int,int> &p1, pair<int,int> &p2){
+      if(p1.first>p2.first) return 1;
+      else if(p1.first == p2.first) return(p1.second<p2.second);
+      return 0;
+    }
